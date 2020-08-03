8dfe2597fc74f0acdf36f84483ab6d798c216b9e
Jason Swadel <65959349+jswadel@users.noreply.github.com>
2020-08-03 14:53:39 -0400

    Update code

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
 setup(name='docassemble.MAFamilyLawComplaintForContempt',
       version='0.0.1',
       description=('A docassemble extension.'),
-      long_description='# docassemble.MAFamilyLawComplaintForContempt\n\nA docassemble extension.\n\n## Author\n\nJason Swadel, jswadel@su.suffolk.edu\n\n',
+      long_description='# docassemble.MAFamilyLawComplaintForContempt\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\nJason Swadel, jswadel@su.suffolk.edu\r\n\r\n',
       long_description_content_type='text/markdown',
       author='Jason Swadel',
       author_email='jswadel@su.suffolk.edu',
